d4e5f067aaa7bb4f773c300b0e3ed1a976ddd193
Iulian Meghea <iulian.meghea@gmail.com>
2014-07-17 14:17:22 +0300

    eventListener typo

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,7 +1,7 @@
 var Polyfills = {
 	eventListener: {
 		test: function(){
-			var winProto = window.prototype;
+			var winProto = Window.prototype;
 			return 'addEventListener' in winProto && 'removeEventListener' in winProto && 'dispatchEvent'
 		}
 	},
@@ -9,24 +9,24 @@ var Polyfills = {
 		test: function(){
 			return 'Promise' in window
 		}
-	},
-	yesop:{
-		test:function(){
-			return true
-		}
-	}
-	,
-	needop:{
-		test :function(){
-			return false;
-		}
-	},
-	noop:{
-		test:function(){
-			return false
-		},
-		needs:['needop']
+	}//,
+	// yesop:{
+	// 	test:function(){
+	// 		return true
+	// 	}
+	// }
+	// ,
+	// needop:{
+	// 	test :function(){
+	// 		return false;
+	// 	}
+	// },
+	// noop:{
+	// 	test:function(){
+	// 		return false
+	// 	},
+	// 	needs:['needop']
 
-	}
+	// }
 }
 module.exports = Polyfills;
