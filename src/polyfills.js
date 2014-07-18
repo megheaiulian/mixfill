b19bbd8616fab2d408150405b22c450974738dae
Iulian Meghea <iulian.meghea@gmail.com>
2014-07-18 14:56:24 +0300

    element matches polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -9,7 +9,29 @@ var Polyfills = {
 		test: function(){
 			return 'Promise' in window
 		}
-	}//,
+	},
+	elementMatches:{
+		test:function(){
+			if (Element){(function(ElementPrototype){
+				ElementPrototype.matches = ElementPrototype.matchesSelector =
+				ElementPrototype.matchesSelector || 
+				ElementPrototype.webkitMatchesSelector ||
+				ElementPrototype.mozMatchesSelector ||
+				ElementPrototype.msMatchesSelector ||
+				ElementPrototype.oMatchesSelector ||
+				function (selector) {
+					var nodes = (this.parentNode || this.document).querySelectorAll(selector), i = -1;
+			 
+					while (nodes[++i] && nodes[i] !== this);
+			 
+					return !!nodes[i];
+				};
+				})(Element.prototype);
+			}
+			return true;
+		}
+	}
+	//,
 	// yesop:{
 	// 	test:function(){
 	// 		return true
