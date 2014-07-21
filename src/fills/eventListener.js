7b4ee65fd7593b4e61aa7b3159eee3648f8409c3
Iulian Meghea <iulian.meghea@gmail.com>
2014-07-21 10:10:00 +0300

    event listener change

--- src/fills/eventListener.js
+++ src/fills/eventListener.js
@@ -1,144 +1,271 @@
-// EventListener | MIT/GPL2 | github.com/jonathantneal/EventListener
-this.Element && Element.prototype.attachEvent && !Element.prototype.addEventListener && (function () {
-	function addToPrototype(name, method) {
-		Window.prototype[name] = HTMLDocument.prototype[name] = Element.prototype[name] = method;
+/** EventTarget support */
+;(function() {
+	if (document.addEventListener || !window.Element || !window.Event) { return; }
+
+	var expando = "__events"; /* own property for storing listeners */
+	var flag = "__immediateStopped"; /* stopImmediatePropagation flag */
+
+	Event.prototype.NONE = Event.NONE = 0;
+	Event.prototype.CAPTURING_PHASE = Event.CAPTURING_PHASE = 1;
+	Event.prototype.AT_TARGET = Event.AT_TARGET = 2;
+	Event.prototype.BUBBLING_PHASE = Event.BUBBLING_PHASE = 3;
+
+	Event.prototype.preventDefault = function() { if (this.cancelable !== false) { this.returnValue = false; } }
+	Event.prototype.stopPropagation = function() { this.cancelBubble = true; }
+	Event.prototype.stopImmediatePropagation = function() { this[flag] = this.cancelBubble = true; }
+
+	var decorate = function(e, listenerNode) { /* improve event properties */
+		e.timeStamp = +new Date();
+		if (!e.target) { e.target = e.srcElement || listenerNode; }
+		e.pageX = e.clientX + document.documentElement.scrollLeft;
+		e.pageY = e.clientY + document.documentElement.scrollTop;
+
+		if (e.type == "mouseover") { 
+			e.relatedTarget = e.fromElement; 
+		} else if (e.type == "mouseout") { 
+			e.relatedTarget = e.toElement; 
+		} else {
+			e.relatedTarget = null;
+		}
+		return e;
 	}
 
-	// add
-	addToPrototype("addEventListener", function (type, listener) {
-		var
-		target = this,
-		listeners = target.addEventListener.listeners = target.addEventListener.listeners || {},
-		typeListeners = listeners[type] = listeners[type] || [];
-
-		// if no events exist, attach the listener
-		if (!typeListeners.length) {
-			target.attachEvent("on" + type, typeListeners.event = function (event) {
-				var documentElement = target.document && target.document.documentElement || target.documentElement || { scrollLeft: 0, scrollTop: 0 };
-
-				// polyfill w3c properties and methods
-				event.currentTarget = target;
-				event.pageX = event.clientX + documentElement.scrollLeft;
-				event.pageY = event.clientY + documentElement.scrollTop;
-				event.preventDefault = function () { event.returnValue = false };
-				event.relatedTarget = event.fromElement || null;
-				event.stopImmediatePropagation = function () { immediatePropagation = false; event.cancelBubble = true };
-				event.stopPropagation = function () { event.cancelBubble = true };
-				event.target = event.srcElement || target;
-				event.timeStamp = +new Date;
-
-				// create an cached list of the master events list (to protect this loop from breaking when an event is removed)
-				for (var i = 0, typeListenersCache = [].concat(typeListeners), typeListenerCache, immediatePropagation = true; immediatePropagation && (typeListenerCache = typeListenersCache[i]); ++i) {
-					// check to see if the cached event still exists in the master events list
-					for (var ii = 0, typeListener; typeListener = typeListeners[ii]; ++ii) {
-						if (typeListener == typeListenerCache) {
-							typeListener.call(target, event);
-
-							break;
-						}
-					}
-				}
-			});
+	/**
+	 * @param {object[]} data
+	 * @param {function} listener
+	 * @param {bool} useCapture
+	 * @returns {number}
+	 */
+	var indexOf = function(data, listener, useCapture) { /* return an index of an existing listener */
+		for (var i=0;i<data.length;i++) {
+			var item = data[i];
+			if (item.useCapture == useCapture && item.listener == listener) { return i; }
 		}
+		return -1;
+	}
 
-		// add the event to the master event list
-		typeListeners.push(listener);
-	});
-
-	// remove
-	addToPrototype("removeEventListener", function (type, listener) {
-		var
-		target = this,
-		listeners = target.addEventListener.listeners = target.addEventListener.listeners || {},
-		typeListeners = listeners[type] = listeners[type] || [];
-
-		// remove the newest matching event from the master event list
-		for (var i = typeListeners.length - 1, typeListener; typeListener = typeListeners[i]; --i) {
-			if (typeListener == listener) {
-				typeListeners.splice(i, 1);
-
-				break;
-			}
-		}
-
-		// if no events exist, detach the listener
-		if (!typeListeners.length && typeListeners.event) {
-			target.detachEvent("on" + type, typeListeners.event);
-		}
-	});
-
-	// dispatch
-	addToPrototype("dispatchEvent", function (eventObject) {
-		var
-		target = this,
-		type = eventObject.type,
-		listeners = target.addEventListener.listeners = target.addEventListener.listeners || {},
-		typeListeners = listeners[type] = listeners[type] || [];
-
-		try {
-			return target.fireEvent("on" + type, eventObject);
-		} catch (error) {
-			if (typeListeners.event) {
-				typeListeners.event(eventObject);
-			}
-
-			return;
-		}
-	});
-
-	// CustomEvent
-	Object.defineProperty(Window.prototype, "CustomEvent", {
-		get: function () {
-			var self = this;
-
-			return function CustomEvent(type, eventInitDict) {
-				var event = self.document.createEventObject(), key;
-
-				event.type = type;
-				for (key in eventInitDict) {
-					if (key == 'cancelable'){
-						event.returnValue = !eventInitDict.cancelable;
-					} else if (key == 'bubbles'){
-						event.cancelBubble = !eventInitDict.bubbles;
-					} else if (key == 'detail'){
-						event.detail = eventInitDict.detail;
-					}
-				}
-				return event;
-			};
-		}
-	});
-
-	// ready
-	function ready(event) {
-		if (ready.interval && document.body) {
-			ready.interval = clearInterval(ready.interval);
-
-			document.dispatchEvent(new CustomEvent("DOMContentLoaded"));
+	var fire = function(event, listener, currentTarget) {
+		event.currentTarget = currentTarget;
+		if (typeof(listener) == "function") {
+			listener.call(currentTarget, event);
+		} else {
+			listener.handleEvent(event);
 		}
 	}
 
-	ready.interval = setInterval(ready, 1);
+	var getAncestors = function(node) {
+		var result = [];
+		while (node.parentNode) {
+			result.unshift(node.parentNode);
+			node = node.parentNode;
+		}
+		return result;
+	}
 
-	window.addEventListener("load", ready);
+	/**
+	 * Run listeners on a nodelist
+	 * @param {Event} event
+	 * @param {node[]} nodes
+	 * @param {number} phase
+	 * @returns {bool} terminated?
+	 */
+	var runListeners = function(event, nodes, phase) {
+		event.eventPhase = phase;
+		for (var i=0;i<nodes.length;i++) {
+			var node = nodes[i];
+			var listeners = [];
+			var data = (node[expando] || {})[event.type] || [];
+
+			for (var j=0;j<data.length;j++) { /* get list of relevant listeners */
+				var item = data[j];
+				if (item.useCapture && phase == Event.BUBBLING_PHASE) { continue; }
+				if (!item.useCapture && phase == Event.CAPTURING_PHASE) { continue; }
+				listeners.push(item.listener);
+			}
+
+			/* trick: use try-catch to gracefully handle failing listeners, but do not use it abundantly */
+			j=0;
+			while (j < listeners.length) {
+				try {
+					while (j < listeners.length) {
+						var listener = listeners[j++];
+						fire(event, listener, node);
+						if (event[flag]) { return true; } /* stopped immediate propagation */
+					}
+				} catch (e) {
+					setTimeout(function() { throw e; }, 0);
+				}
+			}
+
+			if (event.cancelBubble) { return true; } /* stopped propagation */
+		}
+
+		return false; /* propagation not stopped */
+	}
+
+	/**
+	 * The "real" event handler/processor. "this" is {Node} listenerNode.
+	 * @param {Event} event
+	 * @returns {boolean} Not cancelled?
+	 */
+	var handler = function(event) {
+		decorate(event, this);
+
+		var ancestors = getAncestors(event.target);
+		if (ancestors.length) { /* capture */
+			if (runListeners(event, ancestors, Event.CAPTURING_PHASE)) { return event.returnValue; }
+		}
+
+		/* at target */
+		if (runListeners(event, [event.target], Event.AT_TARGET)) { return event.returnValue; }
+
+		if (ancestors.length && event.bubbles !== false) { /* bubble */
+			ancestors.reverse();
+			if (runListeners(event, ancestors, Event.BUBBLING_PHASE)) { return event.returnValue; }
+		}
+
+		event.stopPropagation(); /* do not process natively */
+		return event.returnValue;
+	}
+
+	var proto = {
+		addEventListener: function(type, listener, useCapture) {
+			var data = (this[expando] || {})[type] || [];
+			var count = data.length;
+			if (indexOf(data, listener, useCapture) > -1) { return; } /* already added */
+
+			if (expando in this) {
+				var storage = this[expando];
+			} else {
+				var storage = {
+					_handler: handler.bind(this)
+				};
+				this[expando] = storage;
+			}
+			if (!(type in storage)) { storage[type] = []; }
+			storage[type].push({listener:listener, useCapture:useCapture});
+
+			if (!count) { this.attachEvent("on"+type, storage._handler); } /* first: add native listener */
+		},
+
+		removeEventListener: function(type, listener, useCapture) {
+			var data = (this[expando] || {})[type] || [];
+			var index = indexOf(data, listener, useCapture);
+			if (index == -1) { return; } /* not present */
+
+			data.splice(index, 1);
+			if (!data.length) { this.detachEvent("on"+type, this[expando]._handler); } /* last: remove native listener */
+		},
+
+		dispatchEvent: function(event) {
+			event.returnValue = true;
+			return handler.call(this, event);
+		}
+	}
+
+	var todo = [Element, window.constructor, document.constructor];
+	while (todo.length) {
+		var parent = todo.pop();
+		for (var p in proto) { parent.prototype[p] = proto[p]; }
+	}
 })();
-
-!this.CustomEvent && (function() {
-	// CustomEvent for browsers which don't natively support the Constructor method
-	window.CustomEvent = function CustomEvent(type, eventInitDict) {
-		var event;
-		eventInitDict = eventInitDict || {bubbles: false, cancelable: false, detail: undefined};
-
-		try {
-			event = document.createEvent('CustomEvent');
-			event.initCustomEvent(type, eventInitDict.bubbles, eventInitDict.cancelable, eventInitDict.detail);
-		} catch (error) {
-			// for browsers which don't support CustomEvent at all, we use a regular event instead
-			event = document.createEvent('Event');
-			event.initEvent(type, eventInitDict.bubbles, eventInitDict.cancelable);
-			event.detail = eventInitDict.detail;
+/** CustomEvent polyfill */
+;(function() {
+	if (!window.CustomEvent && document.createEventObject) { /* IE only */
+		window.CustomEvent = function(type, props) {
+			if (!arguments.length) { throw new Error("Not enough arguments"); }
+			var def = {
+				type: type,
+				bubbles: false,
+				cancelable: false,
+				detail: null
+			}
+			var event = document.createEventObject();
+			for (var p in def)   { event[p] = def[p];   }
+			for (var p in props) { event[p] = props[p]; }
+			return event;
 		}
 
-		return event;
-	};
+		return;
+	}
+
+	try {
+		new CustomEvent("test");
+	} catch (e) { /* ctor version not supported or no window.CustomEvent */
+		var CE = function (type, props) {
+			if (!arguments.length) { throw new Error("Not enough arguments"); }
+			var def = {
+				bubbles: false,
+				cancelable: false,
+				detail: null 
+			};
+			for (var p in props)   { def[p] = props[p];   }
+			var event = document.createEvent("CustomEvent");
+			event.initCustomEvent(type, def.bubbles, def.cancelable, def.detail);
+			return event;
+		};
+		
+		CE.prototype = (window.CustomEvent || window.Event).prototype;
+		window.CustomEvent = CE;
+	}
+})();
+;(function(){
+	document.addEventListener("readystatechange", function(e) {
+		if (document.readyState == "complete") { 
+			var event = new CustomEvent("DOMContentLoaded");
+			document.dispatchEvent(event);
+		}
+	});
+})();
+/** MouseEvent polyfill to ease dispatchEvent usage */
+(function() {
+	if (!window.MouseEvent) {
+		window.MouseEvent = function(type, props) {
+			if (!arguments.length) { throw new Error("Not enough arguments"); }
+			var def = {
+				type: type,
+				cancelable: false,
+				bubbles: false
+			}
+			var event = document.createEventObject();
+			for (var p in def)   { event[p] = def[p];   }
+			for (var p in props) { event[p] = props[p]; }
+			return event;
+		}
+		return;
+	}
+
+	try {
+		new MouseEvent("click");
+	} catch (e) {
+		var ME = function(type, props) {
+			if (!arguments.length) { throw new Error("Not enough arguments"); }
+			var def = {
+				type: type,
+				bubbles: false,
+				cancelable: false,
+				view: window,
+				detail: 1,
+				screenX: 0,
+				screenY: 0,
+				clientX: 0,
+				clientY: 0,
+				ctrlKey: false,
+				altLey: false,
+				shiftKey: false,
+				metaKey: false,
+				button: 0,
+				relatedTarget: null
+			}
+			for (var p in props)   { def[p] = props[p];   }
+			var event = document.createEvent("MouseEvent");
+			event.initMouseEvent(
+				def.type, def.bubbles, def.cancelable, def.view, def.detail, def.screenX, def.screenY,
+				def.clientX, def.clientY, def.ctrlKey, def.altKey, def.shiftKey, def.metaKey, def.button, def.relatedTarget
+			);
+			return event;
+		}
+		ME.prototype = window.MouseEvent.prototype;
+		window.MouseEvent = ME;
+	}
 })();
